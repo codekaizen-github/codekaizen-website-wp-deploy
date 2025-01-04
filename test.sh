88f4bcda47c58b90dd3a8d35a42577d791a95fda
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-01-04 08:51:43 -0500

    Get working prototype for WordPress

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
-docker build -t test .
-# docker run --entrypoint /bin/bash -it --rm -v $HOME/.ssh/home_lab_ansible:/root/.ssh/id_rsa test
-docker run --rm -v $HOME/.ssh/home_lab_ansible:/root/.ssh/id_rsa test
+#! /usr/bin/env bash
+docker build -t codekaizen-website-wp-deploy:local-test .
+# docker run --entrypoint /bin/bash -it --rm -v $HOME/.ssh/home_lab_ansible:/root/.ssh/id_rsa codekaizen-website-wp-deploy:local-test
+docker run --rm -v $HOME/.ssh/home_lab_ansible:/root/.ssh/id_rsa codekaizen-website-wp-deploy:local-test
