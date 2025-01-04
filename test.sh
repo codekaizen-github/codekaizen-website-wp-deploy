589a1fb43e84d9e145a9c8acdc3d694e8f5144e4
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-01-04 13:54:36 -0500

    Hide passwords

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 docker build -t codekaizen-website-wp-deploy:local-test .
 # docker run --entrypoint /bin/bash -it --rm -v $HOME/.ssh/home_lab_ansible:/root/.ssh/id_rsa codekaizen-website-wp-deploy:local-test
-docker run --rm -v $HOME/.ssh/home_lab_ansible:/root/.ssh/id_rsa codekaizen-website-wp-deploy:local-test
+docker run --rm --env-file .env -v $HOME/.ssh/home_lab_ansible:/root/.ssh/id_rsa codekaizen-website-wp-deploy:local-test
